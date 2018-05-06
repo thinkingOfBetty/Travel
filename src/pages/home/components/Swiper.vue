bc79893308617bdbd4495b8744308c09ecfca6d6
Betty <asd178335898@qq.com>
2018-05-06 23:30:01 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key='item.id'>
+        <img class="swiperImg" :src='item.imgUrl'>
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true // 使得再往前或者再往后拖的时候不会出现空白
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/a8/31be8a58c2556302.jpg_750x200_0dda540c.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+ .wrapper >>> .swiper-pagination-bullet-active
+  background : #fff
+ .wrapper
+  overflow :hidden
+  width :100%
+  height :0
+  padding-bottom :25%
+  background :#eee
+  .swiperImg
+   width:100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,51 +1,21 @@
 <template>
-    <div class="header">
-      <div class="header-left">
-        <div class="iconfont back-icon">&#xe624;</div>
-      </div>
-      <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        请输入城市游玩主题</div>
-      <div class="header-right">城市
-        <span class="iconfont arrow-icon">&#xe6aa;</span>
-      </div>
-    </div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+  </div>
 </template>
-
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper
+  }
 }
+
 </script>
-<style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.header
-  display: flex
-  line-height: .68rem
-  background: $bgColor
-  color: #fff
-  .header-left
-   width: .64rem
-   float:left
-   .back-icon
-    text-align: center
-    font-size: 0.4rem
-  .header-input
-   padding-left: .1rem
-   flex: 1
-   background: #fff
-   margin-top: 0.12rem
-   height: 0.44rem
-   color: #ddd
-   margin-left: .2rem
-   line-height: .44rem
-   border-radius: .1rem
-  .header-right
-   text-align: center
-   float: right
-   width: 1.24rem
-   .arrow-icon
-    font-size: .24rem
-    margin-left:-.04rem
+<style>
 
 </style>

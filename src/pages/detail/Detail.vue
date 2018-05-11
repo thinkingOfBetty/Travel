017b90ed53b77b37c254904470c54a9f4bea5562
Betty <asd178335898@qq.com>
2018-05-12 00:30:03 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
   <div>
       <detail-banner ></detail-banner>
       <detail-header></detail-header>
-      <div class="test"></div>
+      <div class="content">
+        <detail-list :list="list"></detail-list>
+      </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Deatail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        adult: [{
+          title: '成人三管',
+          adult: [{
+            title: 'abc'
+          }]
+        }, {
+          title: '成人五管'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="list">
+        <div class="item border-bottom" v-for="(item,index) of list" :key="index" >
+            <div class="item-title-icon"></div>
+            <div class="item-title">{{item.title}}</div>
+            <div v-if="item.adult" class="item-adult">
+              <detail-list :list="item.adult"></detail-list>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    'list': Array
+  }
+}
+</script>
+ <style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+.border-bottom
+  &:before
+   border-color:#ccc
+.item
+ position relative
+ .item-title
+  height .8rem
+  line-height .8rem
+  padding-left .5rem
+  font-size .32rem
+ .item-title-icon
+  display: inline-block;
+  position: absolute;
+  width: .36rem;
+  height: .36rem;
+  top: .26rem;
+  left: .1rem;
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+  margin-right: .1rem;
+  background-size: .4rem 3rem;
+ .item-adult
+  padding 0 .2rem
+</style>
